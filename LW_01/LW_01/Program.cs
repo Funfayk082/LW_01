f60b74b87a0e9ed9d988dc3d8e351c9164afe23b
Funfayk082 <Funfayk082@gmail.com>
2023-02-01 20:40:40 +0300

    Новое приложение

---
+++ LW_01/LW_01/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("а) Введите число s: ");
+        int s = int.Parse(Console.ReadLine());
+        Console.Write("   Теперь введите число k: ");
+        int k = int.Parse(Console.ReadLine());
+        int d = s + 1;
+        s = d;
+        k = 2 * s;
+        Console.WriteLine($"   После изменения s имеет значение {s}, а k имеет значение {k}.\n");
+        Console.WriteLine("\n\n");
+        Console.Write("б) Введите число s: ");
+        s = int.Parse(Console.ReadLine());
+        Console.Write("   Теперь введите число k: ");
+        k = int.Parse(Console.ReadLine());
+        d = k - 5;
+        k = 2 * d;
+        s = k - 100;
+        Console.WriteLine($"   После изменения s имеет значение {s}, а k имеет значение {k}.");
+    }
+}
